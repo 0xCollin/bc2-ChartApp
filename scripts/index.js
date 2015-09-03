2c0e4f609ec76eac23fcc2629cd7a8d86fdb6bef
Collin Mutembei <0xt00m@gmail.com>
2015-09-03 15:31:06 +0300

    Add pie chart implementation

--- scripts/index.js
+++ scripts/index.js
@@ -5,6 +5,25 @@ var config = {
 };
 var xPadding = 30;
 var yPadding = 30;
+
+var piedata = [{
+  label: "80 - 100",
+  value: 0,
+  color: "red"
+}, {
+  label: "60 - 80",
+  value: 0,
+  color: "red"
+}, {
+  label: "40 - 60",
+  value: 0,
+  color: "blue"
+}, {
+  label: "0 - 40",
+  value: 0,
+  color: "green"
+}];
+
 var fetchSuccess = '<div class="alert alert-success alert-dismissible" role="alert">'
                       +'<button type="button" class="close" data-dismiss="alert" aria-label="Close">'
                         +'<span aria-hidden="true">&times;</span>'
@@ -88,10 +107,30 @@ $(document).ready(function() {
 
       for(var i = 0; i < config.values.length; i ++) {
           c.beginPath();
-          c.arc(getXPixel(i), getYPixel(data.values[i].Y), 4, 0, Math.PI * 2, true);
+          c.arc(getXPixel(i), getYPixel(config.values[i]), 4, 0, Math.PI * 2, true);
           c.fill();
       }
-    });
+
+      var a = [], b = [], d = [], e = [];
+
+      for (var i = 0; i < config.values.length; i++) {
+        if(config.values[i] > 80 && config.values[i] < 100) {
+          a.push(config.values[i]);
+          piedata[0].value = a.length;
+        }else if(config.values[i] > 60 && config.values[i] < 80) {
+          b.push(config.values[i]);
+          piedata[1].value = b.length;
+        }else if(config.values[i] > 40 && config.values[i] < 60) {
+          d.push(config.values[i]);
+          piedata[2].value = d.length;
+        }else if(config.values[i] > 0 && config.values[i] < 40) {
+          e.push(config.values[i]);
+          piedata[3].value = e.length;
+        }
+      }
+
+      new PieChart("piechartCanvas", piedata);
+});
 
     $('#data-table').bootstrapTable({
       url: jsonUrl,
@@ -206,5 +245,155 @@ $(document).ready(function() {
   c.strokeStyle = '#333';
   c.font = 'italic 8pt sans-serif';
   c.textAlign = "center";
+
+  /*
+  *Pie chart constructor
+  *
+  */
+  function PieChart(canvasId, data) {
+    //User defined Properties
+    this.canvas = document.getElementById(canvasId);
+    this.data = data;
+
+    //constants
+    this.padding = 10;
+    this.legendBorder = 2;
+    this.pieBorder = 5;
+    this.colorLabelSize = 10;
+    this.borderColor = "#555";
+    this.shadowColor = "#777";
+    this.shadowBlur = 10;
+    this.shadowX = 2;
+    this.shadowY = 2;
+    this.font = "12pt Calibri";
+
+    //relationships
+    this.context = this.canvas.getContext("2d");
+    this.legendWidth = this.getLegendWidth();
+    this.legendX = this.canvas.width - this.legendWidth;
+    this.legendY = this.padding;
+    this.pieAreaWidth = (this.canvas.width - this.legendWidth);
+    this.pieAreaHeight = this.canvas.height;
+    this.pieX = this.pieAreaWidth / 2;
+    this.pieY = this.pieAreaHeight / 2;
+    this.pieRadius = (Math.min(this.pieAreaWidth, this.pieAreaHeight) / 2) - (this.padding);
+
+    //draw pie chart
+    this.drawPieBorder();
+    this.drawSlices();
+    this.drawLegend();
+
+  }
+
+  /*
+  *Gets the legend width based on the size of
+  *the label text
+  */
+
+  PieChart.prototype.getLegendWidth = function() {
+    /*
+    *loop through all the labels and determine which
+    *label is the longest. Use this information to
+    *determine the label width
+    */
+    this.context.font = this.font;
+    var labelWidth = 0;
+
+    for (var n = 0; n < this.data.length; n++) {
+      var label = this.data[n].label;
+      labelWidth = Math.max(labelWidth, this.context.measureText(label).width);
+    }
+    return labelWidth + (this.padding * 2) + this.legendBorder + this.colorLabelSize;
+  };
+
+  PieChart.prototype.drawPieBorder = function() {
+    var context = this.context;
+    context.save();
+    context.fillStyle = "white";
+    context.shadowColor = this.shadowColor;
+    context.shadowBlur = this.shadowBlur;
+    context.shadowOffsetX = this.shadowX;
+    context.shadowOffsetY = this.shadowY;
+    context.beginPath();
+    context.arc(this.pieX, this.pieY, this.pieRadius + this.pieBorder, 0, Math.PI * 2, false);
+    context.fill();
+    context.closePath();
+    context.restore();
+  };
+
+  /*
+  *Draws the slices for the pie chart
+  *
+  */
+  PieChart.prototype.drawSlices = function() {
+    var context = this.context;
+    context.save();
+    var total = this.getTotalValue();
+    var startAngle = 0;
+    for (var n = 0; n < this.data.length; n++) {
+      var slices = this.data[n];
+
+      //draw slice
+      var sliceAngle = 2 * Math.PI * slices.value / total;
+      var endAngle = startAngle + sliceAngle;
+
+      context.beginPath();
+      context.moveTo(this.pieX, this.pieY);
+      context.arc(this.pieX, this.pieY, this.pieRadius, startAngle, endAngle, false);
+      context.fillStyle = slices.color;
+      context.fill();
+      context.closePath();
+      startAngle = endAngle;
+    }
+    context.restore();
+  };
+
+  /*
+  *Gets the total value of the labels by looping through
+  *the data and adding up each value
+  *
+  */
+  PieChart.prototype.getTotalValue =  function() {
+    var data = this.data;
+    var total = 0;
+
+    for (var n = 0; n < data.length; n++) {
+      total += data[n].value
+    }
+
+    return total;
+  }
+
+  /*
+  *Draws the legend
+  */
+
+  PieChart.prototype.drawLegend = function() {
+    var context = this.context;
+    context.save();
+    var labelX = this.legendX;
+    var labelY = this.legendY;
+
+    context.strokeStyle = "black";
+    context.lineWidth = this.legendBorder;
+    context.font = this.font;
+    context.textBaseline = "middle";
+
+    for (var n = 0; n < this.data.length; n++) {
+      var  slice = this.data[n]
+
+      //draw legend label
+      context.beginPath();
+      context.rect(labelX, labelY, this.colorLabelSize, this.colorLabelSize);
+      context.closePath();
+      context.fillStyle = slice.color;
+      context.fill();
+      context.stroke();
+      context.fillStyle = "black";
+      context.fillText(slice.label, labelX + this.colorLabelSize + this.padding, labelY + this.colorLabelSize / 2);
+      labelY += this.colorLabelSize + this.padding;
+    }
+    context.restore();
+  };
 
 });
